94cb7a059743e2c29be963de2368707a4ff56ef9
Sam Peacock <peacocsl@mail.uc.edu>
2026-03-23 13:03:49 -0400

    fixing server issue

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
